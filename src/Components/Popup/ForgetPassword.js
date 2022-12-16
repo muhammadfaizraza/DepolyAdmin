01e4d61b4a18e477561923fdb0066c1f3778b93c
hassamkazmi <hassamsyed7@gmail.com>
2022-12-16 15:59:49 +0500

    done402g

--- src/Components/Popup/ForgetPassword.js
+++ src/Components/Popup/ForgetPassword.js
@@ -2,12 +2,41 @@ import React from 'react'
 import { useState } from 'react';
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
+import swal from "sweetalert";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const ForgetPassword = () => {
     const [email, setEmail] = useState('')
+    const history = useNavigate();
+
+    const submit = async (event) => {
+        event.preventDefault();
+        try {
+          const formData = new FormData();
+          formData.append("Email", email);
+         const res = await axios.post(`${window.env.API_URL}/adminpassword/forgot`, formData);
+         const msg = res.data.message
+          swal({
+            title: "Success!",
+            text: msg,
+            icon: "success",
+            button: "OK",
+          });
+          history("/");
+        } catch (error) {
+          const err = error.response.data.message;
+          swal({
+            title: "Error!",
+            text: err,
+            icon: "error",
+            button: "OK",
+          });
+        }
+      };
   return (
     <div>
-      <form>
+      <form onSubmit={submit}>
       <div className="col-sm EmailForget">
                     <FloatingLabel
                       controlId="floatingInput"

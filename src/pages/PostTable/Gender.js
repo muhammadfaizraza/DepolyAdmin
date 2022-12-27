cad3680739712c6e2bdf2d68563eb27be3b4b5c1
hammadaslam10m <muhammadfaizraza48@gmail.com>
2022-12-27 17:47:42 +0500

    done

--- src/pages/PostTable/Gender.js
+++ src/pages/PostTable/Gender.js
@@ -1,62 +1,61 @@
-import React, { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import swal from "sweetalert";
 import axios from "axios";
-import { useNavigate , useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 import TextInputValidation from "../../utils/TextInputValidation";
 import { fetchsexshortcode } from "../../redux/getShortCode/getsexshortcode";
-import { useSelector ,useDispatch } from "react-redux";
-
+import { useSelector, useDispatch } from "react-redux";
 
 const Gender = () => {
- //for error
- const dispatch = useDispatch();
+  //for error
+  const dispatch = useDispatch();
 
- const [Error, setError] = useState("")
- const [ErrorAr, setErrorAr] = useState("")
- const {data:sexshortcode} = useSelector((state) => state.sexshortcode)
+  const [Error, setError] = useState("");
+  const [ErrorAr, setErrorAr] = useState("");
+  const [ErrorAbbrev, setErrorAbbrev] = useState("");
+  const [ErrorAbbrevAr, setErrorAbbrevAr] = useState("");
 
-
+  const { data: sexshortcode } = useSelector((state) => state.sexshortcode);
 
   const [NameEn, setNameEn] = useState("");
   const [NameAr, setNameAr] = useState("");
-  const [shortCode, setshortCode] = useState("");
+
   const [AbbrevEn, setAbbrevEn] = useState("");
   const [AbbrevAr, setAbbrevAr] = useState("");
   const [isLoading, setisLoading] = useState(false);
 
-
   const history = useNavigate();
   const { pathname } = useLocation();
   const [state1, setState] = useState({
-		shortCode: '',
-	});
+    shortCode: "",
+  });
 
-  console.log(sexshortcode,'sexshortcode')
+  console.log(sexshortcode, "sexshortcode");
   useEffect(() => {
-		if (sexshortcode) {
-			setState({
-        shortCode: sexshortcode.length === 0 ? 10 : sexshortcode[0].maxshortCode + 1,
-			});
-		} else {
-		}
-	}, [sexshortcode]);
+    if (sexshortcode) {
+      setState({
+        shortCode:
+          sexshortcode.length === 0 ? 10 : sexshortcode[0].maxshortCode + 1,
+      });
+    } else {
+    }
+  }, [sexshortcode]);
 
   useEffect(() => {
     dispatch(fetchsexshortcode());
   }, [dispatch]);
 
-
   const submit = async (event) => {
     event.preventDefault();
-    setisLoading(true)
+    setisLoading(true);
     try {
       const formData = new FormData();
       formData.append("NameEn", NameEn);
       formData.append("NameAr", NameAr);
       // formData.append("shortCode", shortCode);
-      formData.append("shortCode",state1.shortCode);
+      formData.append("shortCode", state1.shortCode);
       formData.append("AbbrevEn", AbbrevEn);
       formData.append("AbbrevAr", AbbrevAr);
 
@@ -67,55 +66,48 @@ const Gender = () => {
         icon: "success",
         button: "OK",
       });
-      if(pathname === '/gender'){
+      if (pathname === "/gender") {
         history("/genderlist");
       }
-      setisLoading(false)
+      setisLoading(false);
     } catch (error) {
       const err = error.response.data.message[0];
       const err1 = error.response.data.message[1];
       const err2 = error.response.data.message[2];
 
-      console.log(err,'dadasd')
+      console.log(err, "dadasd");
       swal({
         title: "Error!",
-        text: err,err1,err2,
+        text: err,
+        err1,
+        err2,
         icon: "error",
         button: "OK",
       });
-      setisLoading(false)
+      setisLoading(false);
     }
   };
-  
-  const data1 = (JSON.stringify(
-    TextInputValidation(
-      "en",
-      NameEn,
-      "Gender Name English"
-    )
-  ));
+
+  const data1 = JSON.stringify(
+    TextInputValidation("en", NameEn, "Gender Name English")
+  );
 
   const obj = JSON.parse(data1);
-  const data2 = (JSON.stringify(
-    TextInputValidation(
-      "ar",
-      NameAr,
-      "Gender Name Arabic"
-    )
-  ));
+  const data2 = JSON.stringify(
+    TextInputValidation("ar", NameAr, "Gender Name Arabic")
+  );
   const objAr = JSON.parse(data2);
-  const styles = {
-    popup:{
-      color: Error.status === true ? "green" : "red",
- 
-    }
-  };
-  const stylesAr = {
-    popupAr:{
-      color: ErrorAr.status === true ? "green" : "red",
- 
-    }
-  };
+  const data3 = JSON.stringify(
+    TextInputValidation("en", AbbrevEn, "Gender Abbreviation English")
+  );
+
+  const abbrev = JSON.parse(data3);
+  const data4 = JSON.stringify(
+    TextInputValidation("ar", AbbrevAr, "Gender Abbreviation Arabic")
+  );
+  const abbrevar = JSON.parse(data4);
+
+
   return (
     <div className="page">
       <div className="rightsidedata">
@@ -136,17 +128,15 @@ const Gender = () => {
                     onChange={(e) => setNameEn(e.target.value)}
                     name="Name"
                     value={NameEn}
-                    onBlur={() =>
-                      setError(obj)
-
-                    }
+                    onBlur={() => setError(obj)}
                   >
-                    <Form.Control type="text" placeholder="Name" required/>
+                    <Form.Control type="text" placeholder="Name" required />
                   </FloatingLabel>
 
                   <span className="spanForm"> |</span>
-                  <span className={Error.status ? 'success' : 'error'}  
-                  >{Error.message}</span>
+                  <span className={Error.status ? "success" : "error"}>
+                    {Error.message}
+                  </span>
                 </div>
 
                 <div className="col-sm">
@@ -158,88 +148,78 @@ const Gender = () => {
                     name="Name"
                     value={NameAr}
                     style={{ direction: "rtl" }}
-                    onBlur={() =>
-                      setErrorAr(objAr)
-                           
-                     }
+                    onBlur={() => setErrorAr(objAr)}
                   >
-                    <Form.Control type="text" placeholder="اسم" required/>
+                    <Form.Control type="text" placeholder="اسم" required />
                   </FloatingLabel>
-                  <span className={ErrorAr.status ? 'successAr' : 'errorAr'} >{ErrorAr.message}</span>
+                  <span className={ErrorAr.status ? "successAr" : "errorAr"}>
+                    {ErrorAr.message}
+                  </span>
                 </div>
               </div>
 
               <div className="row mainrow">
-                  <div className="col-sm">
-                    <FloatingLabel
-                      controlId="floatingInput"
-                      label="Abbrevation"
-                      className="mb-3"
-                      name="AbbrevEn"
-                      onChange={(e) => setAbbrevEn(e.target.value)}
-                      value={AbbrevEn}
-                    >
-                      <Form.Control
-                        required
-                       
-                        name="AbbrevEn"
-                        type="text"
-                        placeholder="Abbrevation"
-                        onBlur={() =>
-                         setError(obj)
-                              
-                        }
-                      />
-                    </FloatingLabel>
-                 
-                    <span className="spanForm"> |</span>
-                    <span className="error" style={styles.popup}
-                    >{Error.message}</span>
-                  </div>
-
-                  <div className="col-sm">
-                    <FloatingLabel
-                      controlId="floatingInput"
-                      label="اختصار"
-                      className="mb-3 floatingInputAr"
-                      name="AbbrevAr"
-                      style={{ direction: "rtl" }}
-                      onChange={(e) => setAbbrevAr(e.target.value)}
-                      value={AbbrevAr}
-                    >
-                      <Form.Control
-                        name="AbbrevAr"
-                        
-                        type="text"
-                        placeholder="اختصار"
-                        required
-                        onBlur={() =>
-                          setErrorAr(objAr)
-                               
-                         }
-                     
-                      />
-                    </FloatingLabel>
-                    <span className="errorAr" style={stylesAr.popupAr} >{ErrorAr.message}</span>
-                  </div>
-                </div>
-                <div className="row mainrow">
-                  <div className="col-sm">
+                <div className="col-sm">
                   <FloatingLabel
-                      controlId="floatingInput"
-                      label="Short Code"
-                      className="mb-3"
-                      onChange={(e) =>
-                        setState({ ...state1, shortCode: e.target.value })
-                      }
-                    
-                    >
-                      <Form.Control type="number" placeholder="Description" value={state1.shortCode}/>
-                    </FloatingLabel>
-                 
-									
-                  </div>
+                    controlId="floatingInput"
+                    label="Abbrevation"
+                    className="mb-3"
+                    name="AbbrevEn"
+                    onChange={(e) => setAbbrevEn(e.target.value)}
+                    value={AbbrevEn}
+                  >
+                    <Form.Control
+                      required
+                      name="AbbrevEn"
+                      type="text"
+                      placeholder="Abbrevation"
+                      onBlur={() => setErrorAbbrev(abbrev)}
+                    />
+                  </FloatingLabel>
+
+                  <span className="spanForm"> |</span>
+                  <span className={ErrorAbbrev.status ? "success" :"error"}>{ErrorAbbrev.message}</span>
                 </div>
+
+                <div className="col-sm">
+                  <FloatingLabel
+                    controlId="floatingInput"
+                    label="اختصار"
+                    className="mb-3 floatingInputAr"
+                    name="AbbrevAr"
+                    style={{ direction: "rtl" }}
+                    onChange={(e) => setAbbrevAr(e.target.value)}
+                    value={AbbrevAr}
+                  >
+                    <Form.Control
+                      name="AbbrevAr"
+                      type="text"
+                      placeholder="اختصار"
+                      required
+                      onBlur={() => setErrorAbbrevAr(abbrevar)}
+                    />
+                  </FloatingLabel>
+                  <span className={ErrorAbbrev.status ? "successAr" :"errorAr"}>{ErrorAbbrevAr.message}</span>
+                </div>
+              </div>
+              <div className="row mainrow">
+                <div className="col-sm">
+                  <FloatingLabel
+                    controlId="floatingInput"
+                    label="Short Code"
+                    className="mb-3"
+                    onChange={(e) =>
+                      setState({ ...state1, shortCode: e.target.value })
+                    }
+                  >
+                    <Form.Control
+                      type="number"
+                      placeholder="Description"
+                      value={state1.shortCode}
+                    />
+                  </FloatingLabel>
+                </div>
+              </div>
               {/* <div className="row mainrow">
                   <div className="col-sm">
                   <FloatingLabel
@@ -289,7 +269,11 @@ const Gender = () => {
               </div> */}
 
               <div className="ButtonSection " style={{ justifyContent: "end" }}>
-                <button Name="submit" className="SubmitButton" disabled={isLoading}>
+                <button
+                  Name="submit"
+                  className="SubmitButton"
+                  disabled={isLoading}
+                >
                   Add Gender
                 </button>
               </div>
